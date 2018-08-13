3679833e904c5d0e28cb22e87648591e440482c6
Unknown <nm@robawe.com>
2018-08-12 21:35:20 -0500

    add azure policy

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# azure-policy
-Azure Policy Samples
+# Azure Policy Examples
+
+## Prerequisite
+
+Run the following command to register the provider with subscription:
+
+```
+# az provider register --namespace 'Microsoft.PolicyInsights'
+```
+
+## Policy Script
+
+The ```policy.sh``` script shows examples of creating the following policies that are applied to all of the Azure Resources created in other repos in this GitHub account.
+
+* Resource Type Restrictions in the Network Resource Group
+    * Restricts resource types to the following resources:
+        * Virtual Networks
+        * Network Security Groups
+        * Application Security Groups
+* Resource Type Restrictions in the Vault Resource Group
+    * Restricts resources to Key Vaults.
+* Resource Type Restrictions at the Subscription Level
+    * Restricts resources in the Azure Subscription, but excluding the Network and Vault Resource Groups, to the following resources:
+        * Availability Sets
+        * Load Balancers
+        * Public IP Addresses
+        * Storage Accounts
+        * Network Interfaces
+        * Virtual Machines
+        * Disks
+        * Virtual Machine Extensions
+* Resource Location Restrictions at the Subscription Level
+    * Restricts resources in the Azure Subscription to the East US location
+* Tag Enforcement at the Subscription Level
+    * Enforces a requirement that all resources in the Azure Subscription must have a tag named "Cost Center" with the value of "Marketing"
---
+++ policy.sh
@@ -0,0 +1,89 @@
+# Set Variables
+
+```
+SubscriptionId="<subId>"
+NetworkResourceGroupName=RobNetworkRG
+VaultResourceGroupName=VaultRG
+```
+
+# Resource Type Restrictions in the Network Resource Group
+
+```
+# a08ec900-254a-4555-9bf5-e42af04b5c5c is the built-in policy id for Allowed resource types
+az policy assignment create --name 'network-resourcetypes' --scope "/subscriptions/$SubscriptionId/resourceGroups/$NetworkResourceGroupName" --policy "a08ec900-254a-4555-9bf5-e42af04b5c5c" -p '{
+    "listOfResourceTypesAllowed": {
+        "value": [
+            "Microsoft.Network/virtualNetworks",
+            "Microsoft.Network/networkSecurityGroups",
+            "Microsoft.Network/applicationSecurityGroups"
+        ]
+    }
+}'
+```
+
+# Resource Type Restrictions in the Vault Resource Group
+
+```
+az policy assignment create --name 'vault-resourcetypes' --scope "/subscriptions/$SubscriptionId/resourceGroups/$VaultResourceGroupName" --policy "a08ec900-254a-4555-9bf5-e42af04b5c5c" -p '{
+    "listOfResourceTypesAllowed": {
+        "value": [
+            "Microsoft.KeyVault/vaults"
+        ]
+    }
+}'
+```
+
+# Resource Type Restrictions at the Subscription Level
+
+
+```
+az policy assignment create --name 'application-resourcetypes' --scope "/subscriptions/$SubscriptionId" --not-scopes "/subscriptions/$SubscriptionId/resourceGroups/$NetworkResourceGroupName /subscriptions/$SubscriptionId/resourceGroups/$VaultResourceGroupName" --policy "a08ec900-254a-4555-9bf5-e42af04b5c5c" -p '{
+    "listOfResourceTypesAllowed": {
+        "value": [
+            "Microsoft.Compute/availabilitySets",
+            "Microsoft.Network/loadBalancers",
+            "Microsoft.Network/publicIPAddresses",
+            "Microsoft.Storage/storageAccounts",
+            "Microsoft.Network/networkInterfaces",
+            "Microsoft.Compute/virtualMachines",
+            "Microsoft.Compute/disks",
+            "Microsoft.Compute/virtualMachines/extensions"
+        ]
+    }
+}'
+```
+
+# Resource Location Restrictions at the Subscription Level
+
+```
+# e56962a6-4747-49cd-b67b-bf8b01975c4c is the built-in policy id for Allowed locations
+az policy assignment create --name 'east-us-only-location' --scope "/subscriptions/$SubscriptionId" --policy "e56962a6-4747-49cd-b67b-bf8b01975c4c" -p '{
+    "listOfAllowedLocations": {
+        "value": [
+            "East US"
+        ]
+    }
+}'
+```
+
+# Tag Enforcement at the Subscription Level
+
+```
+# 1e30110a-5ceb-460c-a204-c1c3969c6d62 is the built-in policy id for Enforce tag and its value
+az policy assignment create --name 'cost-center-tag-enforcement' --scope "/subscriptions/$SubscriptionId" --policy "1e30110a-5ceb-460c-a204-c1c3969c6d62" -p '{
+    "tagname": {
+        "value": "Cost Center"
+    },
+    "tagvalue": {
+        "value": "Marketing"
+    }
+}'
+```
+
+
+
+
+
+
+
+
